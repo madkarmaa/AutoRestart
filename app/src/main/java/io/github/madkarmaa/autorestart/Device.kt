6265c3464fcef1f04bab3a96f7a53dd3e83df640
madkarmaa <madkarma99@gmail.com>
2025-07-07 15:50:22 +0200

    refactor: re-add attribution

--- app/src/main/java/io/github/madkarmaa/autorestart/Device.kt
+++ app/src/main/java/io/github/madkarmaa/autorestart/Device.kt
@@ -1,3 +1,23 @@
+/*
+ * This file contains code from Hail (https://github.com/aistra0528/Hail)
+ * Hail - Freeze Android apps
+ * Copyright (C) 2021-2024 Aistra
+ * Copyright (C) 2022-2024 Hail contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.madkarmaa.autorestart
 
 import android.content.Context
